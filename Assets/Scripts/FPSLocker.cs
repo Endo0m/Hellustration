f16807ad3702db2d3079c6469eb660132d443d7b
Arthur Sadreev <arthur.sadreev@gmail.com>
2024-11-29 22:30:53 +0300

    last fix

--- Assets/Scripts/FPSLocker.cs
+++ Assets/Scripts/FPSLocker.cs
@@ -1,42 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FPSLocker : MonoBehaviour
 {
-    public int targetFrameRate = 60;
-
-    private float currentFrameTime;
+    [SerializeField] private int targetFrameRate = 60;
 
     void Awake()
     {
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = targetFrameRate;
-    }
-
-    void Start()
-    {
-        StartCoroutine(WaitForNextFrame());
-    }
-
-    IEnumerator WaitForNextFrame()
-    {
-        while (true)
-        {
-            // Начало нового кадра
-            currentFrameTime = Time.realtimeSinceStartup;
-
-            // Ждем завершения всех Update() и LateUpdate()
-            yield return new WaitForEndOfFrame();
-
-            // Вычисляем, сколько времени нужно подождать
-            float timeToWait = (1f / targetFrameRate) - (Time.realtimeSinceStartup - currentFrameTime);
-
-            if (timeToWait > 0)
-            {
-                // Ждем оставшееся время
-                yield return new WaitForSecondsRealtime(timeToWait);
-            }
-        }
     }
 }
